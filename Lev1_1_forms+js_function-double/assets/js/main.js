8ab57aa1974a31f1aef9d0c9b0988e3e194d0235
fahimpjee1988 <55641260+fahimpjee1988@users.noreply.github.com>
2020-06-17 22:55:40 +0200

    Add files via upload

---
+++ Lev1_1_forms+js_function-double/assets/js/main.js
@@ -0,0 +1,24 @@
+var y = document.getElementById("num")
+var x
+function double() {
+    x = y.value * 2
+    document.getElementById("result").value = x
+}
+
+var a = document.getElementById("num1")
+var b
+function calculate() {
+    b = -a.value + 2020
+    document.getElementById("result1").value = b
+}
+
+var c = document.getElementById("num2")
+var d = document.getElementById("num3")
+function calculate() {
+    e = c.value - d.value
+    document.getElementById("result2").value = e
+}
+
+
+
+
